2298c50b9e0ddf87b824c505d641636f1b398a3e
jiakai0419 <jiakai0419@gmail.com>
2016-07-25 20:52:57 +0800

    kael-confgen v1

--- kael-confgen/app/Main.hs
+++ kael-confgen/app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
 import Lib
+import System.IO (stdin, hGetContents)
+import System.Environment (getArgs)
 
 main :: IO ()
-main = someFunc
+main = do
+  [id, groupId] <- map read <$> getArgs
+  urls <- lines <$> hGetContents stdin
+  putStr $ "# group_" ++ show groupId ++ "   pc & m site" ++ "\n"
+  putStr $ gen id groupId urls
--- kael-confgen/src/Lib.hs
+++ kael-confgen/src/Lib.hs
@@ -1,6 +1,10 @@
 module Lib
-    ( someFunc
-    ) where
+  ( gen
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List (intersperse)
+
+gen :: Int -> Int -> [String] -> String
+gen id groupId urls = unlines . map gen' $ zip [id..] urls
+  where p = 100 `div` (length urls)
+        gen' (id, url) = concat . intersperse "\t" . (++[url]) . map show $ [id, groupId, p]
