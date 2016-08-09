071ea81b9f2724e0d56ff63dfcd7231243acfbdd
jiakai0419 <jiakai0419@gmail.com>
2016-08-10 00:07:12 +0800

    monet square circle doodle

--- monet/app/Main.hs
+++ monet/app/Main.hs
@@ -2,13 +2,9 @@
 module Main where
 
 import Lib
-import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
-import Data.Monoid (mconcat)
-
-doodle :: Diagram B
-doodle = mconcat . zipWith rotateBy angles $ repeat (square 1)
-  where angles = map (/360.0) [1.5,3.0..360.0]
 
 main :: IO ()
-main = mainWith doodle
+main = do
+  f <- doodleSCX_
+  mainWith . f $ take 10 [1..]
--- monet/src/Lib.hs
+++ monet/src/Lib.hs
@@ -1,1 +1,28 @@
-module Lib where
+module Lib
+  ( doodleSCX_
+  ) where
+
+import Data.Colour.Palette.ColorSet (Kolor, rybColor)
+import Diagrams.Prelude
+import Data.Monoid (mconcat)
+import System.Random (randoms, newStdGen)
+import Diagrams.Backend.SVG.CmdLine
+
+cut :: Int -> [a] -> [[a]]
+cut n [] = []
+cut n xs = let (a, b) = splitAt n xs in [a] ++ cut n b
+
+paletteIR :: Int -> IO [[Kolor]]
+paletteIR n = cut n . map rybColor . randoms <$> newStdGen
+
+doodleSC :: Double -> [Kolor] -> Diagram B
+doodleSC r colors = mconcat . zipWith lc colors . zipWith rotateBy angles . repeat $ square r
+  where angles = map (/360.0) [1.5,3.0..360.0]
+
+doodleSCX :: [Double] -> [[Kolor]] -> Diagram B
+doodleSCX rs colorss = mconcat $ zipWith doodleSC rs colorss
+
+doodleSCX_ :: IO ([Double] -> Diagram B)
+doodleSCX_ = do
+  colorss <- paletteIR 10000
+  return $ flip doodleSCX colorss
