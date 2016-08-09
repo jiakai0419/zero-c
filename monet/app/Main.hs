8bf4efe6863703ab97ce31505b64f46623cad211
jiakai0419 <jiakai0419@gmail.com>
2016-08-09 19:02:03 +0800

    monet doodle

--- monet/app/Main.hs
+++ monet/app/Main.hs
@@ -4,6 +4,11 @@ module Main where
 import Lib
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
+import Data.Monoid (mconcat)
+
+doodle :: Diagram B
+doodle = mconcat . zipWith rotateBy angles $ repeat (square 1)
+  where angles = map (/360.0) [1.5,3.0..360.0]
 
 main :: IO ()
-main = mainWith (circle 1 :: Diagram B)
+main = mainWith doodle
