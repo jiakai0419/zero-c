ba52a1ea21605a3db4848bbb8d86cb33de9de858
jiakai0419 <jiakai0419@gmail.com>
2016-08-12 22:32:59 +0800

    这个代码处理中文还是有问题的

--- adlog-analyze/app/Main.hs
+++ adlog-analyze/app/Main.hs
@@ -2,11 +2,12 @@ module Main where
 
 import Lib
 import System.IO
+import Data.List (sort)
 
 main :: IO ()
 main = do
-  handle <- openFile "a" ReadMode
-  -- hSetEncoding handle utf8
-  lines <- lines <$> hGetContents handle
-  mapM_ putStrLn lines
-  -- print $ pjsonExtractRT lines
+  lines <- lines <$> hGetContents stdin
+  rtList <- return . pjsonExtractRT $ lines
+  print rtList
+  print . sort $ rtList
+  print . length $ rtList
--- adlog-analyze/src/Lib.hs
+++ adlog-analyze/src/Lib.hs
@@ -1,6 +1,7 @@
 module Lib where
 
 import Text.Regex (matchRegex, mkRegex)
+import Data.Maybe (isJust, fromJust)
 
-pjsonExtractRT :: [String] -> [(Int, Maybe [String])]
-pjsonExtractRT = zip [1..] . map (matchRegex (mkRegex ".*\\[AdvController\\] \\[cpt_new.json\\] \\[SUCCESS\\] ([0-9]+)ms"))
+pjsonExtractRT :: [String] -> [Int]
+pjsonExtractRT = map (read . head . fromJust) . filter isJust . map (matchRegex (mkRegex ".*\\[AdvController\\] \\[cpt_new\\.json\\] \\[SUCCESS\\] ([0-9]+)ms"))
