0de32b77681aa5cdf49fa20972a44b61c860d032
jiakai0419 <jiakai0419@gmail.com>
2016-08-18 22:39:23 +0800

    create lenses by hand

--- tutorial-lens/app/Main.hs
+++ tutorial-lens/app/Main.hs
@@ -2,10 +2,27 @@ module Main where
 
 import Lib
 import System.IO (putStrLn)
+import Lens.Micro hiding (element)
+import Lens.Micro.Extras
 
-atom1 = Atom { _element = "C", _point = Point { _x = 1.0, _y = 2.0}}
-atom2 = Atom { _element = "O", _point = Point { _x = 3.0, _y = 4.0}}
+p1 = Point { _x = 1.0, _y = 2.0}
+p2 = Point { _x = 3.0, _y = 4.0}
+
+atom1 = Atom { _element = "C", _point = p1}
+atom2 = Atom { _element = "O", _point = p2}
+
 molecule = Molecule { _atoms = [atom1, atom2] }
 
 main :: IO ()
-main = putStrLn "Hello World"
+main = do
+  print . view x $ p1
+  print . over x (+1) $ p1
+
+  print . view element $ atom1
+  print . over element (++"ZZ") $ atom1
+
+  print . view atoms $ molecule
+  print . over atoms (++[atom2]) $ molecule
+
+  print . view (atoms . traverse . element) $ molecule
+  print . over (atoms . traverse . point . x) (*2) $ molecule
--- tutorial-lens/src/Lib.hs
+++ tutorial-lens/src/Lib.hs
@@ -5,25 +5,27 @@ import Lens.Micro hiding (element)
 import Lens.Micro.TH
 import Lens.Micro.Extras
 
-data Atom = Atom { _element :: String
-                 , _point :: Point
-                 } deriving (Show)
-
 data Point = Point { _x :: Double
                    , _y :: Double
                    } deriving (Show)
 
+data Atom = Atom { _element :: String
+                 , _point :: Point
+                 } deriving (Show)
+
 data Molecule = Molecule { _atoms :: [Atom] } deriving (Show)
 
-makeLenses ''Atom
 makeLenses ''Point
-makeLenses ''Molecule
 
--- shiftAtomX :: Atom -> Atom
--- shiftAtomX (Atom e (Point x y)) = Atom e (Point (x+1) y)
+-- makeLenses ''Atom
+element :: Lens' Atom String
+element = lens _element (\atom e -> atom { _element = e })
 
-shiftAtomX :: Atom -> Atom
-shiftAtomX = over (point . x) (+1)
+point :: Lens' Atom Point
+point = lens _point (\atom p -> atom {_point = p})
 
-shiftMoleculeX :: Molecule -> Molecule
-shiftMoleculeX = over (atoms . traverse . point . x) (+1)
+-- makeLenses ''Molecule
+atoms :: Functor f => ([Atom] -> f [Atom]) -> Molecule -> f Molecule
+atoms f m = fmap (setter m) $ f (getter m)
+  where getter = _atoms
+        setter m a = m { _atoms = a }
